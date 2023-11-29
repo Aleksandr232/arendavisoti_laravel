15185971f880504c2e4ae8843372aa9abff9dc19
Aleksandr232 <aleksrowing232@gmail.com>
2023-11-29 15:04:13 +0300

    menu

--- resources/views/site/inc/menu.blade.php
+++ resources/views/site/inc/menu.blade.php
@@ -8,20 +8,13 @@
       <li><a class="nav_theme" href="{{ route('snow_removal') }}">Уборка снега с крыш</a></li>
       <li><a class="nav_theme" href="{{ route('technics_bars') }}">Аренда минитрактора</a></li>
     </ul>
-  </li>
-{{-- <li class="menu_des"><a class="nav_theme" href="{{ route('towers_tour') }}">Вышки-туры</a></li>
-<li class="menu_des"><a class="nav_theme" href="{{ route('scaffolding') }}">Строительные леса</a></li> --}}
+</li>
+<li><a class="nav_theme" href="{{ route('posts') }}">Статьи</a></li>
+<li><a class="nav_theme" href="{{ route('blog') }}">Блог</a></li>
 <li class="menu_des"><a class="nav_theme" href="{{ route('technics') }}">Грузоперевозки</a></li>
 <li class="menu_des"><a class="nav_theme" href="{{ route('snow_removal') }}">Уборка снега с крыш</a></li>
 <li class="menu_des"><a class="nav_theme" href="{{ route('technics_bars') }}">Аренда минитрактора</a></li>
 <li><a class="nav_theme" href="{{ route('gallery') }}">Галерея</a></li>
-<li class="menu_sub">
-    <a href="#" class="down">Статьи и блог</a>
-    <ul class="submenu">
-        <li><a class="nav_theme" href="{{ route('posts') }}">Статьи</a></li>
-        <li><a class="nav_theme" href="{{ route('blog') }}">Блог</a></li>
-    </ul>
-</li>
 <li class="menu_des"><a class="nav_theme" href="{{ route('posts') }}">Статьи</a></li>
 <li class="menu_des"><a class="nav_theme" href="{{ route('blog') }}">Блог</a></li>
 <li><a class="nav_theme" href="{{ route('contacts') }}">Контакты</a></li>
